bc4e6562046179d3cf07cabc701075074848031f
griffin-lora <griffin-lora@github.com>
2025-11-05 11:08:13 -0500

    Fix

--- ABS.hpp
+++ ABS.hpp
@@ -8,6 +8,22 @@ class ABS {
     size_t m_capacity = 0;
     size_t m_size = 0;
     T* m_data = nullptr;
+
+    void reserve() {
+        size_t new_capacity = m_capacity == 0 ? 1 : m_capacity * 2;
+
+        T* new_data = static_cast<T*>(::operator new[](sizeof(T) * new_capacity));
+        for (size_t i = 0; i < m_size; i++) {
+            new (&new_data[i]) T{ std::move(m_data[i]) };
+            m_data[i].~T();
+        }
+
+        ::operator delete[](m_data);
+
+        m_capacity = new_capacity;
+        m_data = new_data;
+    }
+
     public:
         ABS() = default;
         explicit ABS(const size_t capacity) : m_capacity(capacity), m_data(static_cast<T*>(::operator new[](sizeof(T) * capacity))) {}
@@ -85,27 +101,15 @@ class ABS {
         }
 
         void push(const T& elem) {
-            // If at max capacity, then reserve new memory
             if (m_size == m_capacity) {
-                size_t new_capacity = m_capacity == 0 ? 1 : m_capacity * 2;
-
-                T* new_data = static_cast<T*>(::operator new[](sizeof(T) * new_capacity));
-                for (size_t i = 0; i < m_size; i++) {
-                    new (&new_data[i]) T{ std::move(m_data[i]) };
-                    m_data[i].~T();
-                }
-
-                ::operator delete[](m_data);
-
-                m_capacity = new_capacity;
-                m_data = new_data;
+                reserve();
             }
 
             new (&m_data[m_size]) T{ elem };
             m_size++;
         }
 
-        T peek() const {
+        [[nodiscard]] T peek() const {
             if (m_size == 0) {
                 throw std::out_of_range("Cannot peek an empty stack");
             }
